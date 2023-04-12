ee20ecc68b492682cf40d144775a8317ecbd89d3
Yan Yan <evitayan@google.com>
2023-04-12 22:56:19 +0000

    sm6150-common: Add feature flag for IPSEC_TUNNEL_MIGRATION
    This commit adds the IPSEC_TUNNEL_MIGRATION feature flag,
    with XFRM_MIGRATE used as the implementation
    
    Bug: 273379472
    Test: build, flash and verify with "adb shell pm list features"
    Test: atest IpSecManagerTunnelTest
    Change-Id: Id2ee3e65183e32ea63f6251ba921e1e4c6b09ed0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -236,7 +236,8 @@ endif
 
 # Network
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
