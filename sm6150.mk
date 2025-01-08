304e11a5e0cb881208589cd6e9b8e38eff851545
Akash Srivastava <akashniki@gmail.com>
2025-01-09 00:57:34 +0530

    sm6150-common: Build some missing LDAC libs
    * Bluetooth libs look for them
    
    Change-Id: I72ac9c1edf6b05947034908339dbe6a19b8088c0
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -94,6 +94,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+PRODUCT_PACKAGES += \
+    libldacBT_abr \
+    libldacBT_enc
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
