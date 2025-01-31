71659ea03a4a220aa8b78aff576818c7af456ce8
Simao Gomes Viana <xdevs23@outlook.com>
2025-01-31 22:49:57 +0800

    sm6150-common: Set appropriate dalvik props for 8GB ram device
    * Based on https://github.com/AospExtended/platform_frameworks_native/commit/14a6b596477f6b688204101f0b1a8f040743e51c
    * Let's use this instead of the old one we've been using before
    
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
@@ -103,6 +100,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+# Dalvik configs
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.heapstartsize=24m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.46 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=48m
 
 # Display
 PRODUCT_PACKAGES += \
