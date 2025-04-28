5df53ae2662344d10edd96ddcc46512a948f6255
Michael Bestas <mkbestas@lineageos.org>
2025-04-28 11:43:56 +0200

    sm61510-common: Migrate Lineage Health to soong_config_set
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -221,6 +221,11 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
