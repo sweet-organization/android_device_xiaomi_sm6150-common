d9d344a36ef7e0ba77710f43f0152179d1056d58
lemezohaib <zhbkhan644@gmail.com>
2025-05-05 11:16:57 +0200

    sm6150-common: Migrate libcameraservice extension lib via soong_config_set
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -94,6 +94,8 @@ PRODUCT_PACKAGES += \
     libldacBT_enc
 
 # Camera
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sm6150)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
